73df60b61c2d17bf736e072add632f4f1d4063e5
Michelzinn <michelbreno5@hotmail.com>
2022-06-22 09:37:33 -0300

    first commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+Homepage feita com base no curso disponivel no youtube
+link: https://www.youtube.com/watch?v=1qH3wAtX4So&t=201s
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  safelist: [{pattern: /^.*$/}],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
